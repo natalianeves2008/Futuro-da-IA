52b7cd7a83ee378faf98a1ef0810dec2d2f7781e
Natalia Neves <146438671+natalianeves2008@users.noreply.github.com>
2025-08-28 11:36:04 -0300

    teste

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const caixaResultado = document.querySelector(".texto-resultado");
+const lista = [item1, item2]
+
+const perguntas = [
+    {
+        enunciado: 
+        "1. A IA deve mostrar como funciona para todos entenderem?"
+        alternativas: ["Sim", "Não"],
+    },
+    {
+        enunciado: 
+        "2. No futuro, qual habilidade será mais importante para as pessoas?"
+        alternativas: ["Criar ideias novas", "Repetir tarefas sempre do mesmo jeito"],
+    },
+    {
+    enunciado: 
+        "3. Quem deve criar regras para o uso da IA?"
+        alternativas: ["O governo", "Apenas as empresas"],
+    },
+    {
+    enunciado: 
+        "4. Como fazer a IA tratar todas as pessoas de forma justa?"
+        alternativas: ["Usar dados variados e de todos os tipos de pessoas", "Usar sempre os mesmos dados antigos"],
+    },
+    {
+    enunciado: 
+        "5. A IA pode decidir sozinha em situações importantes?"
+        alternativas: ["Não, sempre com um humano ajudando", "Sim, pode decidir sozinha"],
+    }
+]
